6cbb72b01427f452c31f4369c46464235d6eb348
infiSTAR <infiSTAR23@gmail.com>
2015-09-14 22:23:32 +0200

    replace code

--- MPMission/CfgExileCustomCode.hpp
+++ MPMission/CfgExileCustomCode.hpp
@@ -1,5 +1,10 @@
 class CfgExileCustomCode 
 {
 	ExileClient_gui_xm8_slide_healthScanner_onOpen = "fixes\ExileClient_gui_xm8_slide_healthScanner_onOpen.sqf";
+	ExileClient_object_item_consume = "fixes\ExileClient_object_item_consume.sqf";
+	ExileClient_object_item_craft = "fixes\ExileClient_object_item_craft.sqf";
 	ExileClient_object_player_event_onKilled = "fixes\ExileClient_object_player_event_onKilled.sqf";
+	ExileClient_util_gear_repackMagazines = "fixes\ExileClient_util_gear_repackMagazines.sqf";
+	ExileClient_util_inventory_replaceMagazine = "fixes\ExileClient_util_inventory_replaceMagazine.sqf";
+	fn_ambientAnim = "fixes\fn_ambientAnim.sqf";
 };
