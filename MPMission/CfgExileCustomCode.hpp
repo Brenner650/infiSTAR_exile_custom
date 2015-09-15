9a62476d6ddea181bc46d59c9ebd0b39fbb3a419
infiSTAR <infiSTAR23@gmail.com>
2015-09-15 10:49:49 +0200

    addin getnaerbyplayers back

--- MPMission/CfgExileCustomCode.hpp
+++ MPMission/CfgExileCustomCode.hpp
@@ -2,4 +2,5 @@ class CfgExileCustomCode
 {
 	ExileClient_gui_xm8_slide_healthScanner_onOpen = "fixes\ExileClient_gui_xm8_slide_healthScanner_onOpen.sqf";
 	ExileClient_object_player_event_onKilled = "fixes\ExileClient_object_player_event_onKilled.sqf";
+	ExileClient_util_world_getNearbyPlayers = "fixes\ExileClient_util_world_getNearbyPlayers.sqf";
 };
